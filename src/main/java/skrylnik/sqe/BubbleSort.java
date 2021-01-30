c7073d58792d8f1e1626873d8dd25c65204d7680
Mikhail Skrylnik <mikhailskrylnik@gmail.com>
2021-01-30 09:45:36 +0300

    New Function (Printing sorted array)

--- src/main/java/skrylnik/sqe/BubbleSort.java
+++ src/main/java/skrylnik/sqe/BubbleSort.java
@@ -14,4 +14,13 @@ public final class BubbleSort {
                     arr[j+1] = temp;
                 }
     }
+
+    public static void printArray(int arr[])
+    {
+        int n = arr.length;
+        for (int i=0; i<n; ++i)
+            System.out.print(arr[i] + " ");
+        System.out.println();
+    }
+
 }
